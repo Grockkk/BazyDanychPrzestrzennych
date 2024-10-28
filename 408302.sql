36d2a5348ffa4f48ffcbb1c44099b9ed43e7efda
Grockkk <dela.kontakt@gmail.com>
2024-10-28 15:50:47 +0100

    create 408302

---
+++ 408302.sql
@@ -0,0 +1,19 @@
+USE AdventureWorksDW2019;
+GO
+
+CREATE PROCEDURE GetCurrencyRatesByYearsAgo
+    @YearsAgo INT
+AS BEGIN
+    SELECT 
+        fcr.*,
+        dc.CurrencyAlternateKey
+    FROM 
+        FactCurrencyRate AS fcr
+    INNER JOIN 
+        DimCurrency AS dc ON fcr.CurrencyKey = dc.CurrencyKey
+    WHERE 
+    CONVERT(DATE, CONVERT(VARCHAR, fcr.DateKey)) <= DATEADD(YEAR, -@YearsAgo, GETDATE())
+    AND dc.CurrencyAlternateKey IN ('GBP', 'EUR')
+END;
+
+EXEC GetCurrencyRatesByYearsAgo @YearsAgo = 8;
