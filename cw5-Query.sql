dbb6e87bff37d3da0ed50f172eb9bf29385307c8
Grockkk <dela.kontakt@gmail.com>
2024-11-16 16:27:29 +0100

    zad5 implementation

---
+++ cw5-Query.sql
@@ -0,0 +1,14 @@
+-- Same dni
+SELECT OrderDate, COUNT(OrderDate) as Orders_cnt 
+FROM FactInternetSales
+GROUP BY OrderDate
+HAVING COUNT(OrderDate) <100
+ORDER BY Orders_cnt DESC;
+
+-- Cena jednostkowa
+SELECT a.OrderDate, a.UnitPrice 
+FROM
+	(SELECT OrderDate, UnitPrice, 
+			ROW_NUMBER() OVER (PARTITION BY OrderDate ORDER BY UnitPrice DESC) as RowNumber
+	 FROM FactInternetSales) a
+WHERE a.RowNumber <=3
